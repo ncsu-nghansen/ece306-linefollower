cf0d487a551990e1a508d56f8f1b8baa6f568110
ncsu-nghansen <nghansen@ncsu.edu>
2014-04-17 13:57:20 -0400

    Working Final Version

--- prgm_control.c
+++ prgm_control.c
@@ -12,6 +12,12 @@ volatile int StateTransitions = 0;
 char IsLight = 1;
 char WasLight = 1;
 
+#define OFF_RIGHT (0)
+#define RIGHT_EDGE (1)
+#define CENTER (2)
+#define LEFT_EDGE (3)
+#define OFF_LEFT (4)
+
 void Control_Process(void)
 {
 	if(ADC_Thumb > 200)  //Disables motors while calibrating
@@ -19,7 +25,7 @@ void Control_Process(void)
 		LeftMotorPower = 0;
 		RightMotorPower = 0;
 		MotorTimer = 0;
-                SecTimer = 180;
+                SecTimer = 120;
 		
 		//          "0123456789abcdef"   
 		display_2 = "Motors  Disabled";
@@ -27,24 +33,78 @@ void Control_Process(void)
 		return;
 	}
 
-	if(MotorTimer == 0)
+	/*if(MotorTimer == 0)
 	{
-		if(IsRightIrLight() && IsLeftIrLight()) //Quickly turn left when off line
-			RightMotorPower = 100, LeftMotorPower = 0, display_2 = "       Light    ", display_1 = "       Light    ";
+		if(IsRightIrLight() && IsLeftIrLight()) //Semi-Quickly turn left when off line
+			RightMotorPower = 40, LeftMotorPower = 0, display_2 = "       Light    ", display_1 = "       Light    ";
 		if(IsRightIrLight() && !IsLeftIrLight()) //Slowly turn left when on edge
-			RightMotorPower = 80, LeftMotorPower = 50, display_2 = "       Light    ", display_1 = "       Dark     ";
+			RightMotorPower = 40, LeftMotorPower = 40, display_2 = "       Light    ", display_1 = "       Dark     ";
 		if(!IsRightIrLight() && !IsLeftIrLight()) //Slowly turn right when on line
-			RightMotorPower = 50, LeftMotorPower = 80, display_2 = "       Dark     ", display_1 = "       Dark     ";
+			RightMotorPower = 30, LeftMotorPower = 80, display_2 = "       Dark     ", display_1 = "       Dark     ";
 		if(!IsRightIrLight() && IsLeftIrLight()) //Quickly turn right when on far edge
 			RightMotorPower = 0, LeftMotorPower = 100, display_2 = "       Dark     ", display_1 = "       Light    ";
-
-		MotorTimer = 1;
-	}
+	}*/
+        
+        switch(ProgramState)
+        {
+          case OFF_RIGHT:
+            if(IsRightIrLight() && !IsLeftIrLight())
+              ProgramState = RIGHT_EDGE;
+            break;
+            
+          case RIGHT_EDGE:
+            if(IsRightIrLight() && IsLeftIrLight())
+              ProgramState = OFF_RIGHT;
+            if(!IsRightIrLight() && !IsLeftIrLight())
+              ProgramState = CENTER;
+            break;
+            
+          case CENTER:
+            if(IsRightIrLight() && !IsLeftIrLight())
+              ProgramState = RIGHT_EDGE;
+            if(!IsRightIrLight() && IsLeftIrLight())
+              ProgramState = LEFT_EDGE;
+            break;
+            
+          case LEFT_EDGE:
+            if(!IsRightIrLight() && !IsLeftIrLight())
+              ProgramState = CENTER;
+            if(IsRightIrLight() && IsLeftIrLight())
+              ProgramState = OFF_LEFT;
+            break;
+            
+          case OFF_LEFT:
+            if(!IsRightIrLight() && IsLeftIrLight())
+              ProgramState = LEFT_EDGE;
+            break;            
+        }
+        
+        if(MotorTimer == 0)
+        {
+          MotorTimer = 1;
+          
+          switch(ProgramState)
+          {
+          case OFF_RIGHT:
+            RightMotorPower = 100, LeftMotorPower = 0,   display_2 = " Off Right Side ";
+            break;
+            
+          case RIGHT_EDGE:         
+          case CENTER:
+          case LEFT_EDGE:
+            RightMotorPower = 80, LeftMotorPower = 80,   display_2 = "     On Line    ";
+            break;
+            
+          case OFF_LEFT:
+            RightMotorPower = 0, LeftMotorPower = 100,   display_2 = "  Off Left Side ";
+            break;
+          }
+        }
 		
-	if(SecTimer == 90)  //If need to turn around
+	if(SecTimer == 60)  //If need to turn around
 	{
 		SecTimer--;
-		timedSpinCW(2000);
+		timedSpinCW(1000);
 	}
         
 	if(SecTimer == 0)
